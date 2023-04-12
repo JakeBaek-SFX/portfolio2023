a609ce07543241d186d2d6cf0674b4134d90b7d6
Jake Baek <jake.baek@soulfx.com>
2023-04-12 17:10:14 -0400

    fix: added cent photos | added side card

--- src/app/views/ProjectDetails.tsx
+++ src/app/views/ProjectDetails.tsx
@@ -4,11 +4,13 @@ import SimpleImageSlider from "react-simple-image-slider";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { colors } from "../settings/colors";
-const project1 = require('../../images/cent_1.png');
-const project2 = require('../../images/copay-home.png');
-const project3 = require('../../images/epay-admin-home.png');
-const project4 = require('../../images/epay-public-home.png');
-const project5 = require('../../images/myrx-home.png');
+const project1 = require('../../images/cent-1.png');
+const project2 = require('../../images/cent-2.png');
+const project3 = require('../../images/cent-3.png');
+const project4 = require('../../images/cent-4.png');
+const project5 = require('../../images/cent-5.png');
+const project6 = require('../../images/cent-6.png');
+const project7 = require('../../images/cent-7.png');
 
 interface IProps {
 
@@ -23,23 +25,59 @@ const ProjectDetails: FC<IProps> = (props) => {
     { url: project3 },
     { url: project4 },
     { url: project5 },
+    { url: project6 },
+    { url: project7 }
   ];
 
   return (
     <ViewTemplate>
-      <div>{project}</div>
-      <SlideWrapper>
-        <SimpleImageSlider
-          width={800}
-          height={400}
-          images={images}
-          showBullets={true}
-          showNavs={true}
-        />
-      </SlideWrapper>
+      <ProjectDetailsWrapper>
+        <SlideWrapper>
+          <SimpleImageSlider
+            width={800}
+            height={400}
+            images={images}
+            showBullets={true}
+            showNavs={true}
+          />
+        </SlideWrapper>
+        <Description>
+          <UpperWrapper>
+          {project}
+          </UpperWrapper>
+          <LowerWrapper>
+          {project}
+          </LowerWrapper>
+        </Description>
+      </ProjectDetailsWrapper>
     </ViewTemplate>
   );
 };
+
+const UpperWrapper = styled.div`
+  background: ${colors.white};
+  border-radius: 10px;
+  box-shadow: 0 3px 3px 3px #EAE0DA;
+  overflow: hidden;
+  border: 3px solid #e5e5e5;
+`
+
+const LowerWrapper = styled.div`
+  background: ${colors.white};
+  border-radius: 10px;
+  box-shadow: 0 3px 3px 3px #EAE0DA;
+  overflow: hidden;
+  border: 3px solid #e5e5e5;
+`
+
+const Description = styled.div`
+  background: ${colors.white};
+  border-radius: 10px;
+  box-shadow: 0 3px 3px 3px #EAE0DA;
+  overflow: hidden;
+  border: 3px solid #e5e5e5;
+  margin-left: 50px;
+`
 
 const SlideWrapper = styled.div`
   background: ${colors.white};
@@ -47,7 +85,10 @@ const SlideWrapper = styled.div`
   box-shadow: 0 3px 3px 3px #EAE0DA;
   overflow: hidden;
   border: 3px solid #e5e5e5;
+`
 
+const ProjectDetailsWrapper = styled.div`
+  display: flex;
 `
 
 export { ProjectDetails } ;
